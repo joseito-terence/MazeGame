ba107d19ed340aa504dc72833ce00b75ef8f6742
Joseito Fernandes <joseito.terence@gmail.com>
2023-08-06 13:19:50 +0530

    vite base for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MazeGame/',
   plugins: [react(), vanillaExtractPlugin()],
   build: {
     target: 'es2017',
